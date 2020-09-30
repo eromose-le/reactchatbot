36568bd744b2830a510486e4b238dd41e22b12b5
eromose-le <erons24@yahoo.com>
2020-09-30 03:06:38 +0100

    bazzcode-api

--- projects/externalapidemo-upio/index.js
+++ projects/externalapidemo-upio/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const { WebhookClient } = require('dialogflow-fulfillment');
 const { Card, Suggestion } = require('dialogflow-fulfillment');
-// const axios = require('axios');
+const axios = require('axios');
 
 
 module.exports = (request, response) => {
