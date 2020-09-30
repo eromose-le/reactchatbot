0041a94948e418d1a14a95b9d2e3bd2aca1c5bd0
eromose-le <erons24@yahoo.com>
2020-09-30 02:03:13 +0100

    bazzcode-api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const asyncHandler = require('express-async-handler');
 const app = express()
 app.use(bodyParser.json())
 //const port = 3000
@@ -8,12 +9,12 @@ app.get("/", (req, res) => {
     res.send({ hello: "Aus TI NE" });
 });
 
-app.post('/dialogflow-fulfillment', (request, response) => {
-    const project = request.body.session.split("/")[1];
+app.post('/dialogflow-fulfillment', asyncHandler(async (request, response) => {
+    let project = await request.body.session.split("/")[1];
     console.log(project);
     const functionToCall = require(`./projects/${project}`)
     functionToCall(request, response)
-})
+}))
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
