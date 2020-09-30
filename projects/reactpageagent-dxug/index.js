360c5cbdb16112bab3dab70de9a0aca8852a8da7
eromose-le <erons24@yahoo.com>
2020-09-30 01:38:36 +0100

    bazzcode-api

--- projects/reactpageagent-dxug/index.js
+++ projects/reactpageagent-dxug/index.js
@@ -1,7 +1,8 @@
+'use strict';
 const functions = require('firebase-functions');
 const { WebhookClient } = require('dialogflow-fulfillment');
 const { Card, Suggestion } = require('dialogflow-fulfillment');
-// const axios = require('axios');
+const axios = require('axios');
 
 
 module.exports = (request, response) => {
@@ -13,7 +14,16 @@ module.exports = (request, response) => {
 
  function rhymingWordHandler(agent) {
   const word = agent.parameters.word;
-  agent.add('intent called: ' + word);
+  agent.add(`Here are the rhyming words for ${word}`);
+  return axios.get(`https://api.datamuse.com/words?rel_rhy=${word}`)
+   .then((result) => {
+    // console.log(result.data);
+    result.data.map(wordObj => {
+     // console.log(wordObj.word);
+     agent.add(wordObj.word);
+    });
+   });
+  // agent.add('intent called: ' + word);
  }
 
  let intentMap = new Map();
