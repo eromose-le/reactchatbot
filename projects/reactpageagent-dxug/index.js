198d8e6fa3529c164af899dba8f3c981a0d0b032
eromose-le <erons24@yahoo.com>
2020-09-30 18:57:40 +0100

    bazzcode-api

--- projects/reactpageagent-dxug/index.js
+++ projects/reactpageagent-dxug/index.js
@@ -29,12 +29,12 @@ module.exports = (request, response) => {
   function rhymingWordHandler(agent) {
     const word = agent.parameters.word;
     agent.add(`Here are the rhyming words for ${word}`);
-    return axios.get(`https://api.datamuse.com/words?rel_rhy=${word}`)
+    axios.get(`https://api.datamuse.com/words?rel_rhy=${word}`)
       .then((result) => {
         console.log(result.data);
         result.data.map(wordObj => {
           console.log(wordObj.word);
-          agent.add(wordObj.word);
+          agent.add(`${wordObj.word}`);
         });
       });
     // agent.add('intent called: ' + word);
