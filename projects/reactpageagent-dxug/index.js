dc6e777c27e9f9b15c408e3408c663ae95ca5c91
eromose-le <erons24@yahoo.com>
2020-10-03 14:33:56 +0100

    bazzcode-api

--- projects/reactpageagent-dxug/index.js
+++ projects/reactpageagent-dxug/index.js
@@ -1,6 +1,17 @@
 'use strict';
 const functions = require('firebase-functions');
 
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../../../reactpageagent-dxug-firebase-adminsdk-26f6q-e1563ff30f.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://reactpageagent-dxug.firebaseio.com"
+});
+
+
 const { WebhookClient } = require('dialogflow-fulfillment');
 const { Card, Suggestion } = require('dialogflow-fulfillment');
 const axios = require('axios');
