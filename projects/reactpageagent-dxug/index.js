263743fc04bf0367a1326c43dfc74ed3d19e355b
eromose-le <erons24@yahoo.com>
2020-10-05 22:32:08 +0100

    bazzcode-api

--- projects/reactpageagent-dxug/index.js
+++ projects/reactpageagent-dxug/index.js
@@ -1,9 +1,6 @@
 'use strict';
 const functions = require('firebase-functions');
-
-
 var admin = require("firebase-admin");
-
 var serviceAccount = require("../../reactpageagent-dxug-firebase-adminsdk-26f6q-e1563ff30f.json");
 
 admin.initializeApp({
@@ -11,12 +8,9 @@ admin.initializeApp({
   databaseURL: "https://reactpageagent-dxug.firebaseio.com"
 });
 
-
 const { WebhookClient } = require('dialogflow-fulfillment');
 const { Card, Suggestion } = require('dialogflow-fulfillment');
 const axios = require('axios');
-
-
 
 module.exports = (request, response) => {
   const agent = new WebhookClient({ request, response });
@@ -28,14 +22,13 @@ module.exports = (request, response) => {
   function rhymingWordHandler(agent) {
     const word = agent.parameters.word;
     agent.add(`Here are the rhyming words for ${word}`)
-    axios.get(`https://api.datamuse.com/words?rel_rhy=${word}`)
+    return axios.get(`https://api.datamuse.com/words?rel_rhy=${word}`)
       .then((result) => {
         console.log(result.data);
         result.data.map(wordObj => {
-          console.log(wordObj.word);
-          agent.add(JSON.stringify(wordObj.word));
-          return;
-          // agent.end(`${wordObj.word}`);
+          var temp = wordObj.word;
+          console.log(temp);
+          agent.add(`Api words: ${temp}`);
         });
       });
   };
