959050da304062dd66c0ae79a278af0b94e1f143
eromose-le <erons24@yahoo.com>
2020-10-07 11:05:41 +0100

    bazzcode-api

--- projects/reactpageagent-dxug/index.js
+++ projects/reactpageagent-dxug/index.js
@@ -23,7 +23,7 @@ module.exports = (request, response) => {
         console.log(result.data);
         result.data.map(wordObj => {
           console.log(wordObj.word);
-          return agent.add(JSON.stringify(wordObj.word)).join(" & ");
+          return agent.add(JSON.stringify(wordObj.word)).join(" \n ");
           // agent.end(`${wordObj.word}`);
         });
       });
